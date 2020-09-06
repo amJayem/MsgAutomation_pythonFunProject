81056a37cc86e28c188fe04b34fb10fba0483992
Jayem <dmjayem@gmail.com>
2020-09-06 06:48:31 +0600

    changes ipa

--- msgAutmation.py
+++ msgAutmation.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC01159837f14638fabafa90a4e597bba6'
-auth_token = '5d3722a928f48de13ed5bf55690d2b45'
+auth_token = '5d3722a928f48de13ed5bf55690amj47'#d2b45'
 client = Client(account_sid, auth_token)
 
 def send_msg():
